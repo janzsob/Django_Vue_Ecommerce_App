a220d82f17f321e0bca1e21c0902b3b9d78ebc13
Bence Janzsó <bence.janzso@gmail.com>
2021-10-08 16:14:00 +0400

    created models

--- E_com/E_com/settings.py
+++ E_com/E_com/settings.py
@@ -46,6 +46,8 @@ INSTALLED_APPS = [
     'rest_framework.authtoken',
     'corsheaders',
     'djoser',
+    # django apps
+    'product'
 
 ]
 
@@ -134,6 +136,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media') # it's where the uploaded images will go
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
