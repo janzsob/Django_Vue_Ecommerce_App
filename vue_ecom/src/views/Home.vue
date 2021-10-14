1ca849ea6f9f189a30ff4067f6217e945f074ff6
Bence Janzsó <bence.janzso@gmail.com>
2021-10-14 17:20:32 +0400

    created product detail view

--- vue_ecom/src/views/Home.vue
+++ vue_ecom/src/views/Home.vue
@@ -21,16 +21,16 @@
         v-for="product in latestProducts"
         v-bind:key="product.id"
       >
-      <div class="box">
-        <figure class="image mb-4">
-          <img :src="product.get_thumbnail">
-        </figure>
+        <div class="box">
+          <figure class="image mb-4">
+            <img :src="product.get_thumbnail">
+          </figure>
 
-        <h3 class="is-size-4">{{ product.name }}</h3>
-        <p class="is-size-6 has-text-grey">${{ product.price }}</p>
+          <h3 class="is-size-4">{{ product.name }}</h3>
+          <p class="is-size-6 has-text-grey">${{ product.price }}</p>
 
-        View details
-      </div>
+          <router-link v-bind:to="product.get_absolute_url" class="button is-dark mt-4">View details</router-link>
+        </div>
 
       </div>
 
